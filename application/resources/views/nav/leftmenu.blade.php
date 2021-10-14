4f7524be8322a13b0604911b0f9d6178a32363ec
VelozX <viralveloz@gmail.com>
2021-10-13 21:48:00 -0500

    v4
    v4

--- application/resources/views/nav/leftmenu.blade.php
+++
@@ -1,7 +0,0 @@
-@if(auth()->user()->is_team)
-@include('nav.leftmenu-team')
-@endif
-
-@if(auth()->user()->is_client)
-@include('nav.leftmenu-client')
-@endif
--- application/resources/views/pages/clients/components/table/wrapper.blade.php
+++
@@ -1,8 +0,0 @@
-<!--main table view-->
-@include('pages.clients.components.table.table')
-
-<!--filter-->
-@if(auth()->user()->is_team)
-@include('pages.clients.components.misc.filter-clients')
-@endif
-<!--filter-->
