a75b006a79e41a40a8653d40b147fa74b5200da8
VelozX <viralveloz@gmail.com>
2021-10-13 21:26:28 -0500

    v2
    v2

--- application/resources/views/pages/contracts/components/table/wrapper.blade.php
+++
@@ -1,9 +0,0 @@
-<!--main table view-->
-@include('pages.tasks.contracts.table.table')
-
-
-<!--filter-->
-@if(auth()->user()->is_team)
-@include('pages.contracts.components.misc.filter-contracts')
-@endif
-<!--filter-->
--- application/resources/views/pages/tickets/components/table/wrapper.blade.php
+++
@@ -1,8 +0,0 @@
-<!--main table view-->
-@include('pages.tickets.components.table.table')
-
-<!--filter-->
-@if(auth()->user()->is_team)
-@include('pages.tickets.components.misc.filter-tickets')
-@endif
-<!--filter-->
